9292113824fb5e44a342ff8d2299d40ef1aa12d9
PrabhaniN <prabhaninanayakkara@gmail.com>
2019-02-10 20:53:10 +0530

    Added customized themes

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 void main(){
   runApp(MaterialApp(
     title: 'Chat App',
+    theme: defaultTargetPlatform == TargetPlatform.android
+      ?kDefaultTheme
+      :kIOSTheme,
     home: ChatScreen(),
   ));
 }
@@ -26,25 +31,36 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
     return Scaffold(
       appBar: AppBar(
         title: Text("Chat App"),
+        elevation:
+          Theme.of(context).platform == TargetPlatform.android ? 4.0 : 0.0
       ),
-      body: new Column(
-        children: <Widget>[
-          new Flexible(
-            child: new ListView.builder(
-              padding: new EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Flexible(
+              child: new ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
             ),
-          ),
-          new Divider(height: 1.0,),
-          new Container(
-            decoration: new BoxDecoration(
-              color: Theme.of(context).cardColor
+            new Divider(height: 1.0,),
+            new Container(
+              decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor
+              ),
+              child: _buildTextComposer(),
+            )
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+          ? new BoxDecoration(
+            border: new Border(
+              top: new BorderSide(color: Colors.grey[200]),
             ),
-            child: _buildTextComposer(),
           )
-        ],
+          : null
       ),
 
       drawer: Drawer(
@@ -73,8 +89,8 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
               },
             )
           ],
-    ),
-  ),
+        ),
+      ),
     );
   }
   Widget _buildTextComposer(){
@@ -100,13 +116,20 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
             ),
             new Container(
               margin: new EdgeInsets.symmetric(horizontal: 4.0),
-              child: new IconButton(
+              child: Theme.of(context).platform == TargetPlatform.iOS ?
+              new CupertinoButton(
+                child: new Text("Send"),
+                onPressed: _isComposing
+                  ? () => _handleSubmitted(_textEditingController.text)
+                  : null,
+              ) :
+              new IconButton(
                 icon: new Icon(Icons.send),
                 onPressed: _isComposing
                 ? () => _handleSubmitted(_textEditingController.text)
                 : null,
               ),
-            ),
+            )
           ],
         ),
       )
@@ -130,25 +153,6 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
     message.animationController.forward();
   }
 }
-  // Widget build(BuildContext context) {
-    // return Scaffold(
-      // appBar: new AppBar(
-        // title: new Text('Chat App'),
-      // ),
-      // body: Center(
-        // child: RaisedButton(
-            // child: Text('Click Here'),
-            // onPressed: (){
-              // Navigator.push(
-                  // context,
-                  // MaterialPageRoute(builder: (context) => SecondRoute())
-              // );
-            // }
-        // ),
-      // ),
-    // );
-  // }
-// }
 
 class SecondRoute extends StatelessWidget{
   @override
@@ -166,6 +170,7 @@ class SecondRoute extends StatelessWidget{
           ),
           RaisedButton(
             child: Text('Go Back'),
+            color: Colors.blue,
             onPressed: (){
               Navigator.pop(context);
             },
@@ -184,7 +189,7 @@ class ChatMessage extends StatelessWidget{
   final AnimationController animationController;
   @override
   Widget build(BuildContext context){
-    return SizeTransition(
+    var sizeTransition = SizeTransition(
       sizeFactor: new CurvedAnimation(
         parent: animationController, curve: Curves.easeOut
       ),
@@ -214,5 +219,17 @@ class ChatMessage extends StatelessWidget{
         ),
       ),
     );
+    return sizeTransition;
   }
 }
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.blue,
+  accentColor: Colors.blue
+);
