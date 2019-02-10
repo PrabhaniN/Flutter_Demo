a68f512829ee043c72b4c6569b750f52c7f5e1d1
PrabhaniN <prabhaninanayakkara@gmail.com>
2019-02-10 19:22:19 +0530

    added animations

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -12,10 +12,15 @@ class ChatScreen extends StatefulWidget{
   State createState() => ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen>{
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textEditingController = new TextEditingController();
   @override
+  void dispose(){
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
+  }
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
@@ -40,7 +45,7 @@ class ChatScreenState extends State<ChatScreen>{
           )
         ],
       ),
-      
+
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -103,10 +108,15 @@ class ChatScreenState extends State<ChatScreen>{
     _textEditingController.clear();
     ChatMessage message = new ChatMessage(
       text: text,
+      animationController: new AnimationController(
+        duration: new Duration(milliseconds: 400),
+        vsync: this,
+      ),
     );
     setState(() {
       _messages.insert(0, message);
     });
+    message.animationController.forward();
   }
 }
   // Widget build(BuildContext context) {
@@ -155,34 +165,41 @@ class SecondRoute extends StatelessWidget{
   }
 }
 
+const String _name = "Your Name";
+
 class ChatMessage extends StatelessWidget{
-  ChatMessage({this.text});
+  ChatMessage({this.text, this.animationController});
   final String text;
+  final AnimationController animationController;
   @override
   Widget build(BuildContext context){
-    return new Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])),
-          ),
-          new Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Text(_name, style: Theme.of(context).textTheme.subhead),
-              new Container(
-                margin: EdgeInsets.only(top: 5.0),
-                child: new Text(text),
-              )
-            ],
-          )
-        ],
+    return SizeTransition(
+      sizeFactor: new CurvedAnimation(
+        parent: animationController, curve: Curves.easeOut
+      ),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0])),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(_name, style: Theme.of(context).textTheme.subhead),
+                new Container(
+                  margin: EdgeInsets.only(top: 5.0),
+                  child: new Text(text),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
-const String _name = "Your Name";
