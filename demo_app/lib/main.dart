7c933b62aac51a60942d6eb9026864709a5accb2
PrabhaniN <prabhaninanayakkara@gmail.com>
2019-02-10 15:07:18 +0530

    Added texting field

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -2,83 +2,113 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    title: 'Navigation',
-    home: FirstRoute(),
+    title: 'Chat App',
+    home: ChatScreen(),
   ));
 }
 
-class FirstRoute extends StatelessWidget{
+class ChatScreen extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First Page'),
-      ),
-      body: Center(
-        child: RaisedButton(
-            child: Text('Click Here'),
-            onPressed: (){
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondRoute())
-              );
-            }
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text("Drawer Header"),
-              decoration: BoxDecoration(
-                color: Colors.blue
-              ),
-            ),
-            ListTile(
-              title: Text("First Page"),
-              onTap: (){
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text("Second Page"),
-              onTap: (){
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: (context) => SecondRoute()),
-                );
-              },
-            )
-          ],
-        ),
-      ),
-    );
-  }
+  State createState() => ChatScreenState();
 }
 
-class SecondRoute extends StatelessWidget{
+class ChatScreenState extends State<ChatScreen>{
+  final TextEditingController _textEditingController = new TextEditingController();
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),
+        title: Text("Chat App"),
       ),
-      body: Column(
-        children: <Widget>[
-          Image(
-            image: AssetImage('Images/flutter.jpeg'),
-            width: 600.0,
-            height: 350.0,
-          ),
-          RaisedButton(
-            child: Text('Go Back'),
-            onPressed: (){
-              Navigator.pop(context);
-            },
-          ),
-        ],
+      body: _buildTextComposer(),
+    );
+  }
+  Widget _buildTextComposer(){
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textEditingController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(
+          hintText: "Send Message"
+        ),
       ),
     );
   }
+  void _handleSubmitted(String text){
+    _textEditingController.clear();
+  }
 }
+  // Widget build(BuildContext context) {
+    // return Scaffold(
+      // appBar: new AppBar(
+        // title: new Text('Chat App'),
+      // ),
+      // body: Center(
+        // child: RaisedButton(
+            // child: Text('Click Here'),
+            // onPressed: (){
+              // Navigator.push(
+                  // context,
+                  // MaterialPageRoute(builder: (context) => SecondRoute())
+              // );
+            // }
+        // ),
+      // ),
+      // drawer: Drawer(
+        // child: ListView(
+          // padding: EdgeInsets.zero,
+          // children: <Widget>[
+            // DrawerHeader(
+              // child: Text("Drawer Header"),
+              // decoration: BoxDecoration(
+                // color: Colors.blue
+              // ),
+            // ),
+            // ListTile(
+              // title: Text("First Page"),
+              // onTap: (){
+                // Navigator.pop(context);
+              // },
+            // ),
+            // ListTile(
+              // title: Text("Second Page"),
+              // onTap: (){
+                // Navigator.push(
+                  // context, 
+                  // MaterialPageRoute(builder: (context) => SecondRoute()),
+                // );
+              // },
+            // )
+          // ],
+        // ),
+      // ),
+    // );
+  // }
+// }
+
+// class SecondRoute extends StatelessWidget{
+  // @override
+  // Widget build(BuildContext context) {
+    // return Scaffold(
+      // appBar: AppBar(
+        // title: Text('Second Page'),
+      // ),
+      // body: Column(
+        // children: <Widget>[
+          // Image(
+            // image: AssetImage('Images/flutter.jpeg'),
+            // width: 600.0,
+            // height: 350.0,
+          // ),
+          // RaisedButton(
+            // child: Text('Go Back'),
+            // onPressed: (){
+              // Navigator.pop(context);
+            // },
+          // ),
+        // ],
+      // ),
+    // );
+  // }
+// }
