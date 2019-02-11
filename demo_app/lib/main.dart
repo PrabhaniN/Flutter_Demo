71c07e17d6f30eeffd621f960dc8b521c85de464
PrabhaniN <prabhaninanayakkara@gmail.com>
2019-02-12 01:03:03 +0530

    Extended grid

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -187,33 +187,38 @@ class HomePage extends StatelessWidget{
         crossAxisCount: 2,
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0/11.0,
-                  child: Image.asset('Images/flutter.jpeg'),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      SizedBox(height: 8.0),
-                      Text('Line')
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        children: _buildGridCards(10)
       ),
     );
+  }
+  List<Card> _buildGridCards(int count){
+    List<Card> cards = List.generate(
+      count, 
+      (int index) => Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0/11.0,
+              child: Image.asset('Images/flutter.jpeg'),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Title'),
+                  SizedBox(height: 8.0),
+                  Text('Line')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ), 
+    );
+    return cards;
   }
 }
 
